0a5dab02c1802439aeaa7ab3a942494554c1efdc
Maxim Matyunin <matyunya@gmail.com>
2020-10-10 18:17:06 +0900

    spinner

--- components/Spinner/Spinner.svelte
+++ components/Spinner/Spinner.svelte
@@ -1,7 +1,4 @@
 <script>
-  const keyTimes = "0; 0.2; 0.8; 1";
-
-  export let dur = "2s";
   export let size = 20;
   export let fill = "default";
 </script>
@@ -26,28 +23,61 @@
     }
   }
   .rotating {
+    will-change: transform;
     animation: rotating 2s linear infinite;
   }
-  
   svg {
     margin: 0 auto;
+  }
+  .collapsing-1 {
+    animation: collapse-1 2s ease-in infinite;
+  }
+  @keyframes collapse-1 {
+    0% {
+      transform: translate(0);
+    }
+    50% {
+      transform: translate(0%, 22%);
+    }
+    100% {
+      transform: translate(0%, 0%);
+    }
+  }
+  .collapsing-2 {
+    animation: collapse-2 2s ease-in infinite;
+  }
+  @keyframes collapse-2 {
+    0% {
+      transform: translate(0);
+    }
+    50% {
+      transform: translate(22%, -14%);
+    }
+    100% {
+      transform: translate(0%, 0%);
+    }
+  }
+  .collapsing-3 {
+    animation: collapse-3 2s ease-in infinite;
+  }
+  @keyframes collapse-3 {
+    0% {
+      transform: translate(0);
+    }
+    50% {
+      transform: translate(-25%, -13%);
+    }
+    100% {
+      transform: translate(0%, 0%);
+    }
   }
 </style>
 
 <svg class="{$$props.class || ''} rotating" width="{size}px" height="{size}px" viewBox="0 0 400 400"  xmlns="http://www.w3.org/2000/svg">
   <g>
-    <ellipse {fill} ry="25" rx="25" id="dot_1" cy="110" cx="200">
-      <animate attributeName="cy" values="200;110;200;200" {keyTimes} {dur} repeatCount="indefinite" />
-      <animate attributeName="cx" values="200;200;200;200" {keyTimes} {dur} repeatCount="indefinite" />
-    </ellipse>
-    <ellipse {fill} ry="25" rx="25" id="dot_2" cy="250" cx="100">
-      <animate attributeName="cy" values="200;250;200;200" {keyTimes} {dur} repeatCount="indefinite" />
-      <animate attributeName="cx" values="200;100;200;200" {keyTimes} {dur} repeatCount="indefinite" />
-    </ellipse>
-    <ellipse {fill} ry="25" rx="25" id="dot_3" cy="250" cx="300">
-      <animate attributeName="cy" values="200;250;200;200" {keyTimes} {dur} repeatCount="indefinite" />
-      <animate attributeName="cx" values="200;300;200;200" {keyTimes} {dur} repeatCount="indefinite" />
-    </ellipse>
+    <circle class="collapsing-1" {fill} r="25" id="dot_1" cy="110" cx="200" />
+    <circle class="collapsing-2" {fill} r="25" id="dot_2" cy="250" cx="100" />
+    <circle class="collapsing-3" {fill} r="25" id="dot_3" cy="250" cx="300" />
   </g>
 </svg>
 
